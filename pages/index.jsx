6ce5284cb9acda3cc072341b579b0db354f3b516
ZiyangWangGit <148592435+ZiyangWangGit@users.noreply.github.com>
2025-01-25 23:59:12 -0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from 'next/router';
 
 export default function Profile() {
   // URL for the profile image
   const profileImageUrl = "/images/User-Profile-PNG-Image.png"; // Corrected path
+  const router = useRouter();
+  const { id, name, email } = router.query;
 
   return (
     <div>
@@ -22,9 +25,9 @@ export default function Profile() {
         style={{ borderRadius: "50%" }} // Rounded corners
       />
       {/* Static profile information */}
-      <h3>Name</h3>
-      <h3>Email</h3>
-      <h3>Student ID</h3>
+      <h3>Name: {name}</h3>
+      <h3>Email: {email}</h3>
+      <h3>School ID: {id}</h3>
     </div>
   );
 }
