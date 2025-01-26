a8a9127242f52a52ca23b53aac05d7b2157ac6aa
ZiyangWangGit <148592435+ZiyangWangGit@users.noreply.github.com>
2025-01-25 23:59:36 -0500

    Update MainNav.jsx

--- components/MainNav.jsx
+++ components/MainNav.jsx
@@ -1,7 +1,22 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function MainNav() {
+  const router = useRouter();
+  const [userInfo, setUserInfo] = useState({ id: "", name: "", email: "" });
+
+  useEffect(() => {
+    if (router.query.id && router.query.name && router.query.email) {
+      setUserInfo({
+        id: router.query.id,
+        name: router.query.name,
+        email: router.query.email,
+      });
+    }
+  }, [router.query]);
+
   return (
     <>
       {/* Navbar component with dark theme and fixed to the top */}
@@ -15,7 +30,7 @@ export default function MainNav() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               {/* Link to the Profile page */}
-              <Link href="/" passHref legacyBehavior>
+              <Link href={`/?id=${userInfo.id}&name=${userInfo.name}&email=${userInfo.email}`} passHref legacyBehavior>
                 <Nav.Link>Profile</Nav.Link>
               </Link>
               {/* Link to the Courses page */}
