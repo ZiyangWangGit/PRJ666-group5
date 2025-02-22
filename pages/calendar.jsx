b9a3a56336166f824cee6ffc608d124e7f3c4f50
Rahimullah Khadim hussain <rahim.aziz555@gmail.com>
2025-02-21 22:00:21 -0500

    calendar.jsx

--- pages/calendar.jsx
+++ pages/calendar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useUser } from "@/context/UserContext";
-import Layout from "@/components/layout";
+import Layout from "@/components/Layout";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
